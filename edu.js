1b79689cd8bc919dc8064f68fb9b9b3be9938020
FFlip <none@example.com>
2025-03-01 17:01:53 +1100

    Update 8

--- edu.js
+++ edu.js
@@ -15,3 +15,22 @@
 
     themeToggle.addEventListener('click', toggleTheme);
     
+
+// ------------------------------------------------------------------------------- //
+
+
+
+    const images = document.querySelectorAll('img'); // Selects all img tags
+    const overlay = document.getElementById('overlay');
+    const focusedImg = document.getElementById('focusedImg');
+
+    images.forEach(img => {
+    img.addEventListener('click', () => {
+        focusedImg.src = img.src;
+        overlay.style.display = 'flex'; // Show the overlay
+    });
+    });
+
+    overlay.addEventListener('click', () => {
+    overlay.style.display = 'none'; // Hide the overlay when clicked
+    });
