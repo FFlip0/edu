99cc5ba1637e633f1ce76c8bc48a2a735060d057
FFlip <none@example.com>
2025-03-15 23:00:58 +1100

    Update 16

--- edu.js
+++ edu.js
@@ -1,3 +1,26 @@
+
+
+
+
+// Image Focus
+const images = document.querySelectorAll('img'); // Select all <img> elements
+const overlay = document.getElementById('overlay');
+const focusedImg = document.getElementById('focusedImg');
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        focusedImg.src = img.src;
+        overlay.style.display = 'flex';
+    });
+});
+
+overlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+
+
+
+/*
 //Theme Toggle
     const themeToggle = document.getElementById('themeToggle');
     const body = document.body;
@@ -15,5 +38,9 @@
 
     themeToggle.addEventListener('click', toggleTheme);
     
-// Image Focus
 
+
+
+
+
+*/
