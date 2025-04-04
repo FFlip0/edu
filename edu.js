14265a6edba1aced7411dd9f851acbd3c3f7364e
FFlip <none@example.com>
2025-04-05 01:17:58 +1100

    Update 21

--- edu.js
+++ edu.js
@@ -19,28 +19,3 @@ overlay.addEventListener('click', () => {
 });
 
 
-
-/*
-//Theme Toggle
-    const themeToggle = document.getElementById('themeToggle');
-    const body = document.body;
-    const themeKey = 'theme'; 
-
-    const savedTheme = localStorage.getItem(themeKey);
-    if (savedTheme === 'dark') {
-    body.classList.add('root2');
-    }
-
-    function toggleTheme() {
-    body.classList.toggle('root2');
-    localStorage.setItem(themeKey, body.classList.contains('root2') ? 'dark' : 'light'); 
-    }
-
-    themeToggle.addEventListener('click', toggleTheme);
-    
-
-
-
-
-
-*/
